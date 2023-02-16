2566813c7f12edb7b83d63d6fc4e31e4f6620e87
Hakan Çevik <hakanxcevik7@gmail.com>
2023-02-16 09:56:23 +0300

    Skeleton of the application completed.

---
+++ app/src/main/java/com/hakancevik/countriesproject/fragment/CountryFragment.kt
@@ -0,0 +1,48 @@
+package com.hakancevik.countriesproject.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.hakancevik.countriesproject.R
+import com.hakancevik.countriesproject.databinding.FragmentCountryBinding
+
+
+class CountryFragment : Fragment() {
+
+    private var _binding: FragmentCountryBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCountryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
